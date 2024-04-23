e5dd41c74d8d95cadab562fef6737fcb85b8959c
Sithum-Perera <sithumudara677@gmail.com>
2024-04-23 20:45:21 +0530

    first commit

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import useRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 dotenv.config();
 
 mongoose
@@ -14,6 +16,11 @@ mongoose
 
 const app= express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
-    console.log('Server Listening on port 3000');
+    console.log('Server Listening on port 3000!');
 });
+
+app.use("/api/user", useRoutes);
+app.use("/api/auth", authRoutes);
